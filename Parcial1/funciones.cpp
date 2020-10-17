403e5416a141995c8fb12d23e1bff0efa5fc10b1
jandresm618 <jandres.mosquera@udea.edu.co>
2020-10-17 16:53:38 -0500

    mejoras en implementacion de movimientos

--- Parcial1/funciones.cpp
+++ Parcial1/funciones.cpp
@@ -7,14 +7,16 @@
 using namespace std;
 
 void movParabolico(float xf,float yf,float xi,float yi,float alpha,float t,float dt){
-    int vox,voy;
-    vox=(xf-xi)/(cos(alpha)*t);
-    voy=(yf-yi+1.5*G*pow(t,2));
+    int vox,voy,vx,vy;
+    vox=(xf-xi)/t;
+    voy=(yf-yi+0.5*G*pow(t,2))/(t);
     cout<<"Velocidad en x: "<<vox<<endl;
     cout<<"Velocidad en y: "<<voy<<endl;
     while(yi>yf && xi>xf){
-        xi=xi+vox*dt;
-        yi=yi+(voy*dt)+(0.5*G*pow(dt,2));
+        vx=vox*cos(alpha);
+        vy=voy*sin(alpha)-G*dt;
+        xi=xi+vx*dt;
+        yi=yi+(vy*dt)+(0.5*G*pow(dt,2));
         cout<<"Posicion x: "<<xi<<" > "<<xf<<endl;
         cout<<"Posicion y: "<<yi<<" > "<<yf<<endl;
     }
--- Parcial1/main.cpp
+++ Parcial1/main.cpp
@@ -7,6 +7,6 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
-    movParabolico(0,5,100000,10,45,3,0.1);
+    movParabolico(0,5,100000,10,45,300,0.1);
     return 0;
 }
